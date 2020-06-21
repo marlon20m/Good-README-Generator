2f060df67d2939d8f711a3469558386ade3b17b0
marlon20m <marlonmora.ndr@gmail.com>
2020-06-21 13:42:26 -0500

    testing to see if the badges and license will display. Readme was previously not displaying in preview mode

--- README.md
+++ README.md
@@ -1,66 +1,66 @@
-#  dssdjs
-                
-      ## Description 
-            
-      jlsdjsd
-                
-                
-      ## Table of Contents (Optional)
-                
-      If your README is very long, add a table of contents to make it easy for users to find what they need.
-                
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [Credits](#credits)
-      * [License](#license)
-      * [Contributing](#Contributing)
-      * [Badges](#Badges)
-      * [Contact](#Contact)
-                
-                
-      ## Installation
-                
+#  Marlon
+          
+## Description 
       
-                
-      ## Usage 
-                
-      djdsljds
-                
-      ## Credits
-                
-      
-                
-                
-                
-      ## License
-                
-      ![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
-                
-      dsjljds'
-                
-      ---
-      
-      🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-                
-      ## Badges
-                
-      ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-                
-      Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-                
-                
-      ## Contributing
-                
-      dljlsd
-                
-      ## Tests
-                
-      j;s
-                
-      ## Contact
-                
-      https://avatars0.githubusercontent.com/u/62806466?v=4
-      null
-                
-      ---
-      © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+This is a dedicated Readme.
+          
+          
+## Table of Contents (Optional)
+          
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+          
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#Contributing)
+* [Badges](#Badges)
+* [Contact](#Contact)
+          
+          
+## Installation
+          
+Node.js LTS.
+          
+## Usage 
+          
+Creating readme files that are professional.
+          
+## Credits
+          
+Marlon Mora
+          
+          
+          
+## License
+          
+![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
+          
+MIT
+          
+---
+
+🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+          
+## Badges
+          
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+          
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+          
+          
+## Contributing
+          
+Marlon Mora
+          
+## Tests
+          
+Passed.
+          
+## Contact
+          
+https://avatars0.githubusercontent.com/u/62806466?v=4
+null
+          
+---
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- index.js
+++ index.js
@@ -71,74 +71,74 @@ function askQuestions() {
 
           console.log(data);
           var text = `
-      #  ${data.title}
-                
-      ## Description 
-            
-      ${data.description}
-                
-                
-      ## Table of Contents (Optional)
-                
-      If your README is very long, add a table of contents to make it easy for users to find what they need.
-                
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [Credits](#credits)
-      * [License](#license)
-      * [Contributing](#Contributing)
-      * [Badges](#Badges)
-      * [Contact](#Contact)
-                
-                
-      ## Installation
-                
-      ${data.installation}
-                
-      ## Usage 
-                
-      ${data.usage}
-                
-      ## Credits
-                
-      ${data.credit}
-                
-                
-                
-      ## License
-                
-      ![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
-                
-      ${data.license}
-                
-      ---
+#  ${data.title}
+          
+## Description 
       
-      🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-                
-      ## Badges
-                
-      ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-                
-      Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-                
-                
-      ## Contributing
-                
-      ${data.contributing}
-                
-      ## Tests
-                
-      ${data.tests}
-                
-      ## Contact
-                
-      ${data.picture}
-      ${data.email}
-                
-      ---
-      © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
-                
-                    `;
+${data.description}
+          
+          
+## Table of Contents (Optional)
+          
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+          
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#Contributing)
+* [Badges](#Badges)
+* [Contact](#Contact)
+          
+          
+## Installation
+          
+${data.installation}
+          
+## Usage 
+          
+${data.usage}
+          
+## Credits
+          
+${data.credit}
+          
+          
+          
+## License
+          
+![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
+          
+${data.license}
+          
+---
+
+🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+          
+## Badges
+          
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+          
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+          
+          
+## Contributing
+          
+${data.contributing}
+          
+## Tests
+          
+${data.tests}
+          
+## Contact
+          
+${data.picture}
+${data.email}
+          
+---
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+          
+              `;
           console.log(text);
           fs.writeFile("README.md", text.trim(), function (err) {
             console.log("complete");
